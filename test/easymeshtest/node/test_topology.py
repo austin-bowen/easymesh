097e32778a8c6dd4eea7bdfe03adf9e4e3e97d3a
Austin Bowen <austin.bowen.314@gmail.com>
2025-07-11 20:55:44 -0400

    Add TestMeshTopologyManager

--- test/easymeshtest/node/test_topology.py
+++ test/easymeshtest/node/test_topology.py
@@ -1,5 +1,49 @@
-from easymesh.node2.topology import get_removed_nodes
+from easymesh.node2.topology import MeshTopologyManager, get_removed_nodes
 from easymesh.specs import MeshNodeSpec, MeshTopologySpec, NodeId
+
+
+class TestMeshTopologyManager:
+    def setup_method(self):
+        self.node1 = MeshNodeSpec(
+            id=NodeId('node1'),
+            connections=[],
+            topics={'topic1', 'topic2'},
+            services=set(),
+        )
+
+        self.node2 = MeshNodeSpec(
+            id=NodeId('node2'),
+            connections=[],
+            topics={'topic1', 'topic3'},
+            services={'service1', 'service2'},
+        )
+
+        self.node3 = MeshNodeSpec(
+            id=NodeId('node3'),
+            connections=[],
+            topics=set(),
+            services={'service1', 'service3'},
+        )
+
+        self.topology = MeshTopologySpec(nodes=[
+            self.node1,
+            self.node2,
+            self.node3,
+        ])
+
+        self.topology_manager = MeshTopologyManager()
+        self.topology_manager.topology = self.topology
+
+    def test_topology_property(self):
+        assert self.topology_manager.topology is self.topology
+
+    def test_get_nodes_listening_to_topic(self):
+        result = self.topology_manager.get_nodes_listening_to_topic('topic1')
+        assert result == [self.node1, self.node2]
+
+    def test_get_nodes_providing_service(self):
+        result = self.topology_manager.get_nodes_providing_service('service1')
+        assert result == [self.node2, self.node3]
 
 
 class TestGetRemovedNodes:
